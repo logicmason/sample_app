dbab2f73addc725493852de9f12a42ab186df4f3
xiaoma <xiaoma@ubuntu-VirtualBox.(none)>
2012-07-23 02:34:09 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'baaa4a264f88f7a5f9cc743300117b676e6e13afab7fb53d96720226d02ece89240feec1c2d03a602ba35672f5451aa4dd409140424c06a4d222d26a5ab8ac9b'
